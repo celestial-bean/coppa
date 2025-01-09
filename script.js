bb9ebbfe2b4e2c9eed55b53e2cd39e24256d043f
celestial-bean <log>
2025-01-09 15:15:49 -0600

    update script.js

--- script.js
+++ script.js
@@ -18,12 +18,10 @@ document.getElementById("emuBird").addEventListener("mouseover", ()=>{
   sound.play()
 })
 document.getElementById("emuBird").addEventListener("click", ()=>{
-
   let sound = new Audio("audio/freeBird.mp3");
   sound.play()
 })
 var val=Math.floor(Math.random()*100)
-alert(val,val!=99)
 if (val!=99){
 document.getElementById("kiwi").remove()
 }
